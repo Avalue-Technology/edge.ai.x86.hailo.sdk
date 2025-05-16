e612b60e35717d16ebbfa99b221bb6ace6084f06
arthur_huang <arthur_huang@avalue.com>
2025-05-16 15:29:54 +0800

    add scripts for demo

---
+++ scripts/start-async-hailo8-yolov11x.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 main.py \
+-c=10 \
+-t=10 \
+-spath=sdk/samples/videos \
+-m=sdk/models/object-detection/yolo/hailo-8-hef/yolov11x.hef \
+-f \
+-l \
+-d \
+--hailo-async
---
+++ scripts/start-async-hailo8-yolov9c.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 main.py \
+-c=10 \
+-t=10 \
+-spath=sdk/samples/videos \
+-m=sdk/models/object-detection/yolo/hailo-8-hef/yolov9c.hef \
+-f \
+-l \
+-d \
+--hailo-async
